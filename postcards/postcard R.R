3bbb2b18bb43f9abd1f8307048f022e9d06bfcee
DATAUNIRIO <duttross@gmail.com>
2020-12-10 16:57:26 -0300

    Create postcard R.R

---
+++ postcards/postcard R.R
@@ -0,0 +1,19 @@
+# https://github.com/seankross/postcards
+# remotes::install_github("seankross/postcards@main")
+
+library(postcards)
+
+---
+  title: "Willkommen!"
+image: "herzl.jpg"
+color1: "#5A59A3"
+color2: "#C66060"
+angle: 130
+links:
+  - YouTube: "https://youtube.com/"
+- Vimeo: "https://vimeo.com/"
+- Twitter: "https://twitter.com/"
+- Email: "mailto:email@email.com"
+output:
+  postcards::onofre
+---
