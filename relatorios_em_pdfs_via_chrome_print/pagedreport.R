77fba7263beb013225c14938fbfd9404df978ab1
DATAUNIRIO <duttross@gmail.com>
2021-01-25 11:55:43 -0300

    Update pagedreport.R

--- relatorios_em_pdfs_via_chrome_print/pagedreport.R
+++ relatorios_em_pdfs_via_chrome_print/pagedreport.R
@@ -1,3 +1,3 @@
-
-
+#https://pagedreport.rfortherestofus.com/reference/paged_windmill.html
+#https://github.com/rfortherestofus/pagedreport/issues/14
 remotes::install_github("rfortherestofus/pagedreport", ref = "main")
