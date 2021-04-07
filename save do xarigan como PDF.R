0760b9880595fc2b2512f521952080a888f25ca8
DATAUNIRIO <duttross@gmail.com>
2021-04-07 16:02:15 -0300

    xaringan como pdf

---
+++ save do xarigan como PDF.R
@@ -0,0 +1,7 @@
+
+#remotes::install_github("jhelvy/xaringanBuilder")
+#remotes::install_github('rstudio/chromote')
+
+
+xaringanBuilder::build_pdf('C:/Users/Hp/Documents/GitHub/Mobilidade_Urbana/apresentacao/extra-great-slides-mobilidade/intro/index.Rmd')
+
